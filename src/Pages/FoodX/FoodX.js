797edaf53c6f256ed7a6670e3c006826885ea5a8
charliepoker <iheanachocharlie@gmail.com>
2022-08-31 13:32:47 +0100

    fix

--- src/Pages/FoodX/FoodX.js
+++ src/Pages/FoodX/FoodX.js
@@ -110,10 +110,8 @@ function FoodX() {
           <img src={"./phone2.png"} alt="" className="app__info_phone" />
         </div>
         <div className="app__info2_text">
-          <h3>Quick updating of items on sale</h3>
-          <p className="app__info2_heading">
-            Monitor deliveries and orders all from one place
-          </p>
+          <h3>STOCK UPDATING</h3>
+          <p className="app__info2_heading">Quick updating of items on sale</p>
           <p className="app__info2_content">
             Lorem ipsum dolor sit amet, consectetur adipiscing Aliquam vel neque
             nec ex tempus varius mi in purus Cras sit amet di issim enim.
@@ -150,6 +148,55 @@ function FoodX() {
               </svg>
             </span>{" "}
           </h6>
+        </div>
+      </div>
+
+      <div className="app__info__container">
+        <div className="app__info_text">
+          <h3>CUSTOMER FEEDBACK</h3>
+          <p className="app__info_heading">
+            Check your customers favourites and know what they like
+          </p>
+          <p className="app__info_content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing Aliquam vel neque
+            nec ex tempus varius mi in purus Cras sit amet di issim enim.
+            Loremipsumsit amet consectetur adipiscing elit. Aliquam vel neque
+            nec varius mi in purus maximus vulputate. Cras sit ameconsectetur
+            adipiscing elit. Aliquam vel neque nec.
+          </p>
+          <h6>
+            Explore this feature{" "}
+            <span>
+              <svg
+                width="24"
+                height="14"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M14.4297 5.92993L20.4997 11.9999L14.4297 18.0699"
+                  stroke="#000"
+                  stroke-width="1.5"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+                <path
+                  d="M3.5 12H20.33"
+                  stroke="#000"
+                  stroke-width="1.5"
+                  stroke-miterlimit="10"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </span>{" "}
+          </h6>
+        </div>
+        <div className="app__info_img">
+          <div className="app__info_circle"></div>
+          <img src={"./phone3.png"} alt="" className="app__info_phone" />
         </div>
       </div>
     </>
